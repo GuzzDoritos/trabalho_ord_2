6d117a5a33f7ab1c77bc39fe4fe1c24b01065386
Isabella Barela Venancio <barelaisabella25@gmail.com>
2026-05-10 21:21:57 -0300

    fiz a parte de arquivos de indices

--- programa.py
+++ programa.py
@@ -1,7 +1,101 @@
 from sys import argv
-import struct
+from struct import pack, unpack, calcsize
 
 arquivo = "games.dat"
+
+#CONSTANTES
+FORMATO_PRI = "iq"  #2 inteiros de 4 bytes e 8 bytes # i = 4 bytes, q = 8 bytes
+FORMATO_TAM = 'h'   # 2 bytes 
+FORMATO_IDX = 'i'   # 4 bytes 
+FORMATO_INV = 'qii' #3 inteiros de 8bytes e 4 bytes e 4 bytes # q = 8 bytes, i = 4 bytes
+
+SIZEOF_PRI = calcsize(FORMATO_PRI)
+SIZEOF_TAM = calcsize(FORMATO_TAM)
+SIZEOF_INV = calcsize(FORMATO_INV)
+
+#==================INDICE EM ARQUIVO =======================
+    
+def arq_indice_prim(indice_pri): #msm lógica do arq_indice_listainv
+    #12 bytes por entrada: 4 (id) + 8 (offset)
+    ##id (chave) + referencia (byteoffset) = indice_pri
+    if indice_pri is not None:  # salva
+        arq = open("primario.ind", "wb")
+        for id, offset in indice_pri:
+            arq.write(pack(FORMATO_PRI, int(id), offset))
+    else:  # carrega
+        indice_pri = []
+        arq = open("primario.ind", 'rb')
+        while True:
+            dados = arq.read(SIZEOF_PRI)
+            if len(dados) < SIZEOF_PRI:
+                break
+            id, offset = unpack(FORMATO_PRI, dados)
+            indice_pri.append([str(id), offset])
+        return indice_pri
+
+def arq_indice_genero(indice_sec_genero):
+    #2 bytes (tamanho) + N bytes (gênero) + 4 bytes (índice)
+    if indice_sec_genero is not None:  # salva
+        arq = open("genero.ind", "wb")
+        for genero, indice in indice_sec_genero.items():
+            genero_bytes = genero.encode('utf-8') #string para bytes
+            arq.write(pack(FORMATO_TAM, len(genero_bytes)))# tamanho do gênero
+            arq.write(genero_bytes)# gênero
+            arq.write(pack(FORMATO_IDX, indice))# indice na lista inv
+    else:  # carrega
+        indice_sec_genero = {}
+        arq = open("genero.ind", "rb")
+        while True:
+            tam = arq.read(SIZEOF_TAM)
+            if len(tam) < SIZEOF_TAM:
+                break
+            tam = unpack(FORMATO_TAM, tam)[0]
+            genero = arq.read(tam).decode('utf-8') #bytes para string
+            indice = unpack(FORMATO_IDX, arq.read(4))[0]
+            indice_sec_genero[genero] = indice
+        return indice_sec_genero
+
+def arq_indice_publicadora(indice_sec_publicadora):
+    #2 bytes (tamanho) + N bytes (publicadora) + 4 bytes (índice)
+    if indice_sec_publicadora is not None:  # salva
+        arq = open("publicadora.ind", "wb")
+        for publicadora, indice in indice_sec_publicadora.items():
+            publicadora_bytes = publicadora.encode('utf-8') #string para bytes
+            arq.write(pack(FORMATO_TAM, len(publicadora_bytes)))# tamanho da publicadora
+            arq.write(publicadora_bytes)# publicadora
+            arq.write(pack(FORMATO_IDX, indice))# indice na lista inv
+    else:  # carrega
+        indice_sec_publicadora = {}
+        arq = open("publicadora.ind", "rb")
+        while True:
+            tam = arq.read(SIZEOF_TAM)
+            if len(tam) < SIZEOF_TAM:
+                break
+            tam = unpack(FORMATO_TAM, tam)[0]
+            publicadora = arq.read(tam).decode('utf-8') #bytes para string
+            indice = unpack(FORMATO_IDX, arq.read(4))[0]
+            indice_sec_publicadora[publicadora] = indice
+        return indice_sec_publicadora
+
+def arq_indice_listainv(lista_inv): #msm lógica do arq_indice_prim
+    ## 8 bytes (offset) + 4 bytes (prox_genero) + 4 bytes (prox_publicadora) = 16 bytes
+    ##[byte_offset, prox_genero, prox_publicadora] = lista_inv
+    if lista_inv is not None:  # salva
+        arq = open("listaInvertida.lst", "wb")
+        for offset, prox_genero, prox_publicadora in lista_inv:
+            arq.write(pack(FORMATO_INV, offset, prox_genero, prox_publicadora))
+    else:  # carrega
+        lista_inv = []
+        arq = open("listaInvertida.lst", 'rb')
+        while True:
+            dados = arq.read(SIZEOF_INV)
+            if len(dados) < SIZEOF_INV:
+                break
+            offset, prox_genero, prox_publicadora = unpack(FORMATO_INV, dados)
+            lista_inv.append([offset, prox_genero, prox_publicadora])
+        return lista_inv
+
+#===========================MAIN==============================
 
 def main():
     '''Esse programa lê os dados gravados no arquivo criado pelo programa
@@ -24,6 +118,21 @@ def main():
     busca_sec_genero("Action", indicesec[0], lista_inv, ENTRADA)
     busca_sec_publicadora("Electronic Arts", indicesec[1], lista_inv, ENTRADA)
 
+    arq_indice_prim(indicepri)
+    carregado = arq_indice_prim(None)
+    print(carregado)
+
+    arq_indice_genero(indicesec[0])
+    carregado2 = arq_indice_genero(None)
+    print(carregado2)
+
+    arq_indice_publicadora(indicesec[1])
+    carregado3 = arq_indice_publicadora(None)
+    print(carregado3)
+
+    arq_indice_listainv(lista_inv)
+    carregado4 = arq_indice_listainv(None)
+    print(carregado4)
 
     print(ENTRADA.tell())
     print(registros)
@@ -201,8 +310,6 @@ def busca_sec_publicadora(chave, indice_sec_publicadora, lista_inv, arquivo):
 def insercao(chave, num_bytes):
     print(f'Inserção do registro de chave "{chave}" ({num_bytes} bytes)')
 
-
-
 #================REMOCAO==================
 
 def remocao(chave, offset):
@@ -219,16 +326,13 @@ if __name__ == "__main__":
 
 #O QUE FALTA
 '''
-Argumentos da linha de comando (-b, -e, -c)
-Gravar os índices em arquivos (primario.ind, genero.ind, publicadora.ind, listaInvertida.lst)
-Arquivo de operações
 Inserção
 Remoção
 Compactação
+
 Sobrescrever índices no -b
 Persistência dos índices no -e
 Falta ignorar removidos na construção dos índices no -b
-Estrutura dos arquivos de índice
-Tratamento obrigatório quando índices não existirem
-Ver se vai utilizar o struct e busca binária
+Tratamento obrigatório quando índices não existirem no -e
+ver se vai arq.close() ou with as
 '''
